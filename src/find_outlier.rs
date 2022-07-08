7b36afac479563a718d097e6f477fcbc72b31500
khadiev <khadiev.edem@gmail.com>
2022-07-08 16:22:43 +0300

    add find_outlier task impl

---
+++ src/find_outlier.rs
@@ -0,0 +1,23 @@
+pub fn find_outlier(values: &[i32]) -> i32 {
+    let (mut evens, mut odds, mut parity, mut x) = (0, 0, 0, 0);
+
+    for i in 0..3 {
+        if values[i] % 2 == 0 {
+            evens += 1;
+        } else {
+            odds += 1;
+        }
+    }
+
+    if evens > odds {
+        parity = 1
+    }
+
+    for value in values.into_iter() {
+        x = *value;
+        if (x % 2).abs() == parity {
+            break;
+        }
+    }
+    x
+}
