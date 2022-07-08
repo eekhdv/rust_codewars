ca6761cb32fc836e10025f835faba584497079fe
khadiev <khadiev.edem@gmail.com>
2022-07-08 16:23:24 +0300

    format code

--- src/divisors.rs
+++ src/divisors.rs
@@ -1,9 +1,13 @@
 pub fn divisors(integer: u32) -> Result<Vec<u32>, String> {
     let mut v_u: Vec<u32> = Vec::new();
-	for i in 2..(integer / 2 + 1) {
+    for i in 2..(integer / 2 + 1) {
         if integer % i == 0 {
             v_u.push(i);
         }
     }
-    if v_u.is_empty() { Err(format!("{} is prime", integer)) } else { Ok(v_u) }
+    if v_u.is_empty() {
+        Err(format!("{} is prime", integer))
+    } else {
+        Ok(v_u)
+    }
 }
