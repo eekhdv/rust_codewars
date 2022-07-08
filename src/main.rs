b1608c3cae24883d922ee2834b02d7935eb37eb5
khadiev <khadiev.edem@gmail.com>
2022-07-08 16:27:06 +0300

    comment module using

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,20 @@
+use divisors::run;
+
 mod divisors;
 mod is_square;
 mod open_or_senior;
+mod find_next_square;
+mod find_outlier;
 
 fn main() {
-    // call functions here
+    /* call functions here: */
+    /* 
+    * divisors::run(integer);
+    * is_square::run(n);
+    * open_or_senior;:run(integer);
+    * find_next_square::run(sq);
+    * find_outlier::run(values);
+    */
 }
 
 
