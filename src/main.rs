7d3cac88fa7bf6dd547b6329b377c27255f451ab
khadiev <khadiev.edem@gmail.com>
2022-07-17 05:41:18 +0300

    add modules to main

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+use core::num;
+
 use divisors::run;
 
 mod divisors;
@@ -5,16 +7,12 @@ mod is_square;
 mod open_or_senior;
 mod find_next_square;
 mod find_outlier;
+mod split_string;
+mod trailing_zeros;
+mod closest_mult_ten;
+mod number_of_divisions;
 
 fn main() {
-    /* call functions here: */
-    /* 
-    * divisors::run(integer);
-    * is_square::run(n);
-    * open_or_senior;:run(integer);
-    * find_next_square::run(sq);
-    * find_outlier::run(values);
-    */
 }
 
 
